011a482add2242c3a47ed84b3ddec4e24bee4507
Alexandre Delplace <alexandre.delplacemille@gmail.com>
2013-07-30 22:26:20 +0200

    work validatorCreation()

--- forms.js
+++ forms.js
@@ -1,13 +1,13 @@
 ($)(function(){
-//	application.initForm.process();
-	
-	$.fn.validateIt = function(conf) {
-		console.log(conf);
+	$.fn.validateIt = function(param) {
 		var _Form = new Object();
 		var _Valid = true;
 		var _Tmp_value = new Array();
 		
+		validatorCreation(param);
+		
 		$('.form').each(function () {
+			//_Valid = true;
 			_Form = $(this);
 			$('.formSubmit',$(this)).click(function () {
 				$('input:not([type=button]), .form select', _Form).each(function () {
@@ -17,7 +17,7 @@
 						var data = el.attr('data-form').split(" ");
 						for (var x in data) {
 							if (valid) {
-								var obj = window[data[x].replace(/^\s+|\s+$/g, '')];
+								var obj = window['vi_' + data[x].replace(/^\s+|\s+$/g, '')];
 								if (typeof obj == "object") {
 									if (typeof obj.process == "function") {
 										switch(obj._Name) {
@@ -58,7 +58,7 @@
 				var data = el.attr('data-form').split(" ");
 				for (var x in data) {
 					if (valid) {
-						var obj = window[data[x].replace(/^\s+|\s+$/g, '')];
+						var obj = window["vi_" + data[x].replace(/^\s+|\s+$/g, '')];
 						if (typeof obj == "object") {
 							if (typeof obj.process == "function") {
 								if (el.hasClass('error')) {
@@ -80,6 +80,31 @@
 		});
 	}
 	
-	$(document).validateIt()
+	validatorCreation = function (param) {
+		for(var name in param) {
+			window[name] = validator.clone(name);
+			var obj = window[name];
+			for(var fct in param[name]) {
+				obj._Functions.push("require" + fct[0].toUpperCase() + fct.slice(1));
+				//obj.eval("_Message_" + fct) = fct.message;
+			}
+			console.log(obj);
+		}
+	}
+	
+	$(document).validateIt({
+		"lol" : {
+			regex : {
+				conf : "",
+				message: ""
+			},
+			field : {
+				message: ""
+			},
+			optionnalTest : function(self) {
+				
+			}
+		}
+	});
 });
 
--- validator.js
+++ validator.js
@@ -1,15 +1,13 @@
-// Classe "abstraite" de validation
 var validator = {
-    // Paramètres du champs testé
+	
     _Name: "",
     _Value: null,
     _Message: "",
     _Valid: true,
 
-    // Liste des fonctions prédéfinies (sous forme de chaîne) à exécuter sur le champs
     _Functions: [],
-
-    // Messages d'erreur des tests prédéfinis
+	_Message: [],
+	
     _Message_maxLength: "Ce champ possède trop de caractères",
     _Message_minLength: "Ce champ ne possède pas assez de caractères",
     _Message_regex: "Ce champ n'est pas sous le bon format",
@@ -17,7 +15,6 @@ var validator = {
     _Message_mismatch: "Ce champ n'est pas correct",
     _Message_equality: "Ce champ n'est pas correct",
 
-    // Configurations par défaut des test
     _Conf_equality: "",
     _Conf_length: [3, 0],
     _Conf_regex: "",
@@ -37,9 +34,9 @@ var validator = {
             if (!this._Valid)
                 return false;
         }
-        this.optionalTest(this);
+        this.requireOptionalTest(this);
     },
-    optionalTest: function (self) {
+    requireOptionalTest: function (self) {
     },
     requireEquality: function (self) {
         if (self._Value != self._Conf_equality) {
@@ -78,46 +75,47 @@ var validator = {
             self._Valid = false;
         }
     }
+	
 }
 
-var alphanumeric = validator.clone("alphanumeric");
-var alpha = validator.clone("alpha");
-var numeric = validator.clone("numeric");
-var email = validator.clone("email");
-var phoneFR = validator.clone("phoneFR");
-var required = validator.clone("required");
+var vi_alphanumeric = validator.clone("alphanumeric");
+var vi_alpha = validator.clone("alpha");
+var vi_numeric = validator.clone("numeric");
+var vi_email = validator.clone("email");
+var vi_phoneFR = validator.clone("phoneFR");
+var vi_required = validator.clone("required");
 
-var password = validator.clone("password");
-var passwordConfirm = validator.clone("passwordConfirm");
+var vi_password = validator.clone("password");
+var vi_passwordConfirm = validator.clone("passwordConfirm");
 
 /******************* Définition des paramètres *******************/
 
-alphanumeric._Conf_regex = '^[a-zA-Z0-9àáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]*$';
-alphanumeric._Message_regex = 'Ce champ ne doit contenir que des caractères alphanumériques';
-alphanumeric._Functions = ['requireRegex'];
+vi_alphanumeric._Conf_regex = '^[a-zA-Z0-9àáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]*$';
+vi_alphanumeric._Message_regex = 'Ce champ ne doit contenir que des caractères alphanumériques';
+vi_alphanumeric._Functions = ['requireRegex'];
 
-alpha._Conf_regex = '^[a-zA-Zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]*$';
-alpha._Message_regex = 'Ce champ ne doit contenir que des caractères alphabétiques';
-alpha._Functions = ['requireRegex'];
+vi_alpha._Conf_regex = '^[a-zA-Zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]*$';
+vi_alpha._Message_regex = 'Ce champ ne doit contenir que des caractères alphabétiques';
+vi_alpha._Functions = ['requireRegex'];
 
-numeric._Conf_regex = '^[0-9]*$';
-numeric._Message_regex = 'Ce champ ne doit contenir que des caractères numériques';
-numeric._Functions = ['requireRegex'];
+vi_numeric._Conf_regex = '^[0-9]*$';
+vi_numeric._Message_regex = 'Ce champ ne doit contenir que des caractères numériques';
+vi_numeric._Functions = ['requireRegex'];
 
-email._Conf_regex = '^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$';
-email._Message_regex = 'Veuillez entrer un adresse email valide';
-email._Functions = ['requireRegex'];
+vi_email._Conf_regex = '^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$';
+vi_email._Message_regex = 'Veuillez entrer un adresse email valide';
+vi_email._Functions = ['requireRegex'];
 
-phoneFR._Conf_regex = '^[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}$';
-phoneFR._Message_regex = 'Veuillez entrer un numéro de téléphone valide';
-phoneFR._Functions = ['requireRegex'];
+vi_phoneFR._Conf_regex = '^[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}$';
+vi_phoneFR._Message_regex = 'Veuillez entrer un numéro de téléphone valide';
+vi_phoneFR._Functions = ['requireRegex'];
 
-required._Functions = ['requireField'];
+vi_required._Functions = ['requireField'];
 
-password._Conf_regex = '[A-Za-zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-].*[0-9]|[0-9].*[A-Za-zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]$';
-password._Message_regex = "Le mot de passe doit contenir au moins un chiffre et une lettre";
-password._Functions = ['requireRegex'];
+vi_password._Conf_regex = '[A-Za-zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-].*[0-9]|[0-9].*[A-Za-zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]$';
+vi_password._Message_regex = "Le mot de passe doit contenir au moins un chiffre et une lettre";
+vi_password._Functions = ['requireRegex'];
 
-passwordConfirm._Message_equality = "Veuillez entrer le même mot de passe";
-passwordConfirm._Functions = ['requireEquality'];
+vi_passwordConfirm._Message_equality = "Veuillez entrer le même mot de passe";
+vi_passwordConfirm._Functions = ['requireEquality'];
 
