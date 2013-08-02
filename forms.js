abddb8d46d5a62419906a836ab47e233990ee52e
AlexandreDelplace <adelplace@FFXBCT1.interne.odiso.net>
2013-08-02 16:59:18 +0200

    End validatorCreation()

--- forms.js
+++ forms.js
@@ -1,110 +1,116 @@
-($)(function(){
-	$.fn.validateIt = function(param) {
-		var _Form = new Object();
-		var _Valid = true;
-		var _Tmp_value = new Array();
-		
-		validatorCreation(param);
-		
-		$('.form').each(function () {
-			//_Valid = true;
-			_Form = $(this);
-			$('.formSubmit',$(this)).click(function () {
-				$('input:not([type=button]), .form select', _Form).each(function () {
-					var valid = true;
-					if ($(this).attr('data-form') != null) {
-						var el = $(this);
-						var data = el.attr('data-form').split(" ");
-						for (var x in data) {
-							if (valid) {
-								var obj = window['vi_' + data[x].replace(/^\s+|\s+$/g, '')];
-								if (typeof obj == "object") {
-									if (typeof obj.process == "function") {
-										switch(obj._Name) {
-											case 'password':
-												_Tmp_value['password'] = el.val();
-												break;
-											case 'passwordConfirm':
-												passwordConfirm._Conf_equality =_Tmp_value['password'];
-												break;
-											default:
-												break;
-										}
-										if (el.hasClass('error')) {
-											el.next().remove();
-											el.removeClass('error');
-										}
-										obj._Value = el.val().replace(/^\s+|\s+$/g, '');
-										obj.process();
-										if (!obj._Valid) {
-											valid = false;
-											_Valid = false;
-											el.addClass('error');
-											el.after('<small class="error">' + obj._Message + '</small>');
-										}
-									}
-								}
-							}
-						}
-					}
-				});
-				if (_Valid) _Form.submit();
-			});
-		});
-		$('input:not([type=button]), .form select', _Form).on('blur keyup', function () {
-			var valid = true;
-			if ($(this).attr('data-form') != null) {
-				var el = $(this);
-				var data = el.attr('data-form').split(" ");
-				for (var x in data) {
-					if (valid) {
-						var obj = window["vi_" + data[x].replace(/^\s+|\s+$/g, '')];
-						if (typeof obj == "object") {
-							if (typeof obj.process == "function") {
-								if (el.hasClass('error')) {
-									el.next().remove();
-									el.removeClass('error');
-								}
-								obj._Value = el.val().replace(/^\s+|\s+$/g, '');
-								obj.process();
-								if (!obj._Valid) {
-									valid = false;
-									el.addClass('error');
-									el.after('<small class="error">' + obj._Message + '</small>');
-								}
-							}
-						}
-					}
-				}
-			}
-		});
-	}
-	
-	validatorCreation = function (param) {
-		for(var name in param) {
-			window[name] = validator.clone(name);
-			var obj = window[name];
-			for(var fct in param[name]) {
-				obj._Functions.push("require" + fct[0].toUpperCase() + fct.slice(1));
-				//obj.eval("_Message_" + fct) = fct.message;
-			}
-			console.log(obj);
-		}
-	}
-	
-	$(document).validateIt({
-		"lol" : {
-			regex : {
-				conf : "",
-				message: ""
-			},
-			field : {
-				message: ""
-			},
-			optionnalTest : function(self) {
-				
-			}
-		}
-	});
+($)(function() {
+    $.fn.validateIt = function(param) {
+        var _Form = $(this);
+        var _Valid = true;
+        var _Tmp_value = new Array();
+
+        validatorCreation(param);
+
+        $('.form').each(function() {
+            _Form = $(this);
+            $('.formSubmit', $(this)).click(function() {
+                $('input:not([type=button]), .form select', _Form).each(function() {
+                    var valid = true;
+                    if ($(this).attr('data-form') != null) {
+                        var el = $(this);
+                        var data = el.attr('data-form').split(" ");
+                        for (var x in data) {
+                            if (valid) {
+                                var obj = window['vi_' + data[x].replace(/^\s+|\s+$/g, '')];
+                                if (typeof obj == "object") {
+                                    if (typeof obj.process == "function") {
+                                        switch (obj._Name) {
+                                            case 'password':
+                                                _Tmp_value['password'] = el.val();
+                                                break;
+                                            case 'passwordConfirm':
+                                                passwordConfirm._Conf_equality = _Tmp_value['password'];
+                                                break;
+                                            default:
+                                                break;
+                                        }
+                                        if (el.hasClass('error')) {
+                                            el.next().remove();
+                                            el.removeClass('error');
+                                        }
+                                        obj._Value = el.val().replace(/^\s+|\s+$/g, '');
+                                        obj.process();
+                                        if (!obj._Valid) {
+                                            valid = false;
+                                            _Valid = false;
+                                            el.addClass('error');
+                                            el.after('<small class="error">' + obj._Message + '</small>');
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+                if (_Valid)
+                    _Form.submit();
+            });
+        });
+        $('input:not([type=button]), .form select', _Form).on('onBlur keyup', function() {
+            var valid = true;
+            if ($(this).attr('data-form') != null) {
+                var el = $(this);
+                var data = el.attr('data-form').split(" ");
+                for (var x in data) {
+                    if (valid) {
+                        var obj = window["vi_" + data[x].replace(/^\s+|\s+$/g, '')];
+                        if (typeof obj == "object") {
+                            if (typeof obj.process == "function") {
+                                if (el.hasClass('error')) {
+                                    el.next().remove();
+                                    el.removeClass('error');
+                                }
+                                obj._Value = el.val().replace(/^\s+|\s+$/g, '');
+                                obj.process();
+                                if (!obj._Valid) {
+                                    valid = false;
+                                    el.addClass('error');
+                                    el.after('<small class="error">' + obj._Message + '</small>');
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    };
+
+    validatorCreation = function(param) {
+        for (var name in param) {
+            window["vi_" + name] = validator.clone(name);
+            var obj = window["vi_" + name];
+            for (var fct in param[name]) {
+                if (fct == "test") {
+                    obj.requireOptionalTest = param[name][fct];
+                } else {
+                    obj._Functions.push("require" + fct[0].toUpperCase() + fct.slice(1));
+                    if (param[name][fct].message != undefined)
+                        obj._Messages[fct] = param[name][fct].message;
+                    if (param[name][fct].conf != undefined)
+                        obj._Conf[fct] = param[name][fct].conf;
+                }
+            }
+        }
+    };
+
+    $('.form').validateIt({
+        lol: {
+            mismatch: {
+              conf: ["lol","fuck"],
+              message: "lol"
+            },
+            field: {
+                message: "fsefesfefs"
+            },
+            test: function(self) {
+                //alert('lol');
+            }
+        }
+    });
 });
 
--- validator.js
+++ validator.js
@@ -1,32 +1,32 @@
 var validator = {
-	
     _Name: "",
     _Value: null,
     _Message: "",
     _Valid: true,
-
     _Functions: [],
-	_Message: [],
-	
-    _Message_maxLength: "Ce champ possède trop de caractères",
-    _Message_minLength: "Ce champ ne possède pas assez de caractères",
-    _Message_regex: "Ce champ n'est pas sous le bon format",
-    _Message_require: "Veuillez remplir ce champ",
-    _Message_mismatch: "Ce champ n'est pas correct",
-    _Message_equality: "Ce champ n'est pas correct",
+    
+    _Messages: new Array(),
+    _Conf: new Array(),
+    init: function() {
+        this._Messages["minLength"] = "Ce champ ne possède pas assez de caractères";
+        this._Messages["maxLength"] = "Ce champ possède trop de caractères";
+        this._Messages["regex"] = "Ce champ n'est pas sous le bon format";
+        this._Messages["field"] = "Veuillez remplir ce champ";
+        this._Messages["mismatch"] = "Ce champ n'est pas correct";
+        this._Messages["equality"] = "Ce champ n'est pas correct";
 
-    _Conf_equality: "",
-    _Conf_length: [3, 0],
-    _Conf_regex: "",
-    _Conf_mismatch: [],
-
-    clone: function (name) {
-        var obj = $.extend({}, validator);
+        this._Conf["equality"] = "";
+        this._Conf["minLength"] = 0;
+        this._Conf["maxLength"] = 32;
+        this._Conf["mismatch"] = new Array();
+    },
+    clone: function(name) {
+        var obj = $.extend(true,{}, validator);
         obj._Name = name;
+        obj.init();
         return obj;
     },
-
-    process: function () {
+    process: function() {
         this._Message = "";
         this._Valid = true;
         for (var x in this._Functions) {
@@ -36,47 +36,50 @@ var validator = {
         }
         this.requireOptionalTest(this);
     },
-    requireOptionalTest: function (self) {
+    requireOptionalTest: function(self) {
     },
-    requireEquality: function (self) {
-        if (self._Value != self._Conf_equality) {
+    requireEquality: function(self) {
+        if (self._Value != self._Conf["equality"]) {
             self._Valid = false;
-            self._Message = self._Message_equality;
+            self._Message = self._Messages["equality"];
         }
     },
-    requireMismatch: function (self) {
-        for (var x in self._Conf_mismatch) {
-            if (self._Value == self._Conf_mismatch[x]) {
+    requireMismatch: function(self) {
+        for (var x in self._Conf.mismatch) {
+            if (self._Value == self._Conf["mismatch"][x]) {
                 self._Valid = false;
-                self._Message = self._Message_mismatch;
+                self._Message = self._Messages["mismatch"];
             }
         }
     },
-    requireRegex: function (self) {
-        var regex = new RegExp(self._Conf_regex, "g");
+    requireRegex: function(self) {
+        var regex = new RegExp(self._Conf["regex"], "g");
         if (!regex.test(self._Value)) {
             self._Valid = false;
-            self._Message = self._Message_regex;
+            self._Message = self._Messages["regex"];
         }
     },
-    requireField: function (self) {
+    requireField: function(self) {
         if (self._Value.length == 0) {
             self._Valid = false;
-            self._Message = self._Message_require
+            self._Message = self._Messages["field"];
         }
     },
-    requireLength: function (self) {
-        if (self._Value.length < self._Conf_length[0]) {
-            self._Message = self._Message_minLength;
+    requireMinLength: function(self) {
+        if (self._Value.length < self._Conf["minLength"]) {
+            self._Message = self._Messages["minLength"];
             self._Valid = false;
         }
-        if (self._Conf_length[1] != 0 && self._Value.length > self._Conf_length[1]) {
-            self._Message = self._Message_maxLength;
+    },
+    requireMaxLength: function(self) {
+        if (self._Value.length > self._Conf["maxLength"]) {
+            self._Message = self._Messages["maxLength"];
             self._Valid = false;
         }
     }
-	
-}
+
+};
+
 
 var vi_alphanumeric = validator.clone("alphanumeric");
 var vi_alpha = validator.clone("alpha");
@@ -90,32 +93,32 @@ var vi_passwordConfirm = validator.clone("passwordConfirm");
 
 /******************* Définition des paramètres *******************/
 
-vi_alphanumeric._Conf_regex = '^[a-zA-Z0-9àáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]*$';
-vi_alphanumeric._Message_regex = 'Ce champ ne doit contenir que des caractères alphanumériques';
+vi_alphanumeric._Conf["regex"] = '^[a-zA-Z0-9àáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]*$';
+vi_alphanumeric._Messages["regex"] = 'Ce champ ne doit contenir que des caractères alphanumériques';
 vi_alphanumeric._Functions = ['requireRegex'];
 
-vi_alpha._Conf_regex = '^[a-zA-Zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]*$';
-vi_alpha._Message_regex = 'Ce champ ne doit contenir que des caractères alphabétiques';
+vi_alpha._Conf["regex"] = '^[a-zA-Zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]*$';
+vi_alpha._Messages["regex"] = 'Ce champ ne doit contenir que des caractères alphabétiques';
 vi_alpha._Functions = ['requireRegex'];
 
-vi_numeric._Conf_regex = '^[0-9]*$';
-vi_numeric._Message_regex = 'Ce champ ne doit contenir que des caractères numériques';
+vi_numeric._Conf["regex"] = '^[0-9]*$';
+vi_numeric._Messages["regex"] = 'Ce champ ne doit contenir que des caractères numériques';
 vi_numeric._Functions = ['requireRegex'];
 
-vi_email._Conf_regex = '^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$';
-vi_email._Message_regex = 'Veuillez entrer un adresse email valide';
+vi_email._Conf["regex"] = '^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$';
+vi_email._Messages["regex"] = 'Veuillez entrer un adresse email valide';
 vi_email._Functions = ['requireRegex'];
 
-vi_phoneFR._Conf_regex = '^[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}$';
-vi_phoneFR._Message_regex = 'Veuillez entrer un numéro de téléphone valide';
+vi_phoneFR._Conf["regex"] = '^[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}$';
+vi_phoneFR._Messages["regex"] = 'Veuillez entrer un numéro de téléphone valide';
 vi_phoneFR._Functions = ['requireRegex'];
 
 vi_required._Functions = ['requireField'];
 
-vi_password._Conf_regex = '[A-Za-zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-].*[0-9]|[0-9].*[A-Za-zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]$';
-vi_password._Message_regex = "Le mot de passe doit contenir au moins un chiffre et une lettre";
+vi_password._Conf["regex"] = '[A-Za-zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-].*[0-9]|[0-9].*[A-Za-zàáâãäåçèéêëìíîïğòóôõöùúûüıÿ-]$';
+vi_password._Messages["regex"] = "Le mot de passe doit contenir au moins un chiffre et une lettre";
 vi_password._Functions = ['requireRegex'];
 
-vi_passwordConfirm._Message_equality = "Veuillez entrer le même mot de passe";
+vi_passwordConfirm._Messages["equality"] = "Veuillez entrer le même mot de passe";
 vi_passwordConfirm._Functions = ['requireEquality'];
 
