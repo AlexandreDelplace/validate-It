b0d54ece213366ad36e354179304dfb8eabb8a2b
alexandredelplace <alexandre.delplacemille@gmail.com>
2013-08-22 14:27:55 +0200

    Add script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+($)(function() {
+    $('.form').validateIt({
+	check: {
+	    test: function(self) {
+		if (self._Value.indexOf('lol') != -1) {
+		    self._Valid = false;
+		    self._Message = "loelsfejijofseijfoesojf";
+		}
+	    }
+	},
+	prenom: {
+	    mismatch: {
+		conf: ["Jean", "Bernard"],
+		message: "Le prénom ne peut étre égal à Jean ou Bernard"
+	    },
+	    field: {
+		message: "Veuillez entrer votre prénom"
+	    },
+	    test: function(self) {
+		var valid = true;
+		valid = (self._Value != "toto");
+		if (!valid) {
+		    self._Valid = false;
+		    self._Message = "Ce champ est invalide";
+		}
+
+	    }
+	},
+	submit: function(form) {
+	    console.log(form);
+	}
+    });
+    
+    $('.form2').validateIt();
+});
